a01bf9af3b0cba06699c0e8fe7f644ba47ab5c57
raftouch <rafanelkin@gmail.com>
2025-05-05 09:57:33 +0200

    update main script

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 import json
-# import webbrowser
+import webbrowser
 
 
 ### DATA FROM JSON ###
@@ -15,25 +15,27 @@ figureForRegion.write_html("sales-by-region.html")
 figureForRegion.show()
 
 
-# with open('calculation_results/ventes_par_produit.json') as file:
-#     data = json.load(file)
+with open('calculation_results/ventes_par_produit.json') as file:
+    data = json.load(file)
 
-# figureForProduct = px.pie(data, values='ventes_par_produit', names='produit', title='Ventes par produit')
-# figureForProduct.write_html("sales-by-product.html")
-# figureForProduct.show()
+figureForProduct = px.pie(data, values='ventes_par_produit', names='produit', title='Ventes par produit')
+figureForProduct.write_html("sales-by-product.html")
+figureForProduct.show()
 
 
 ### DATA FROM CSV ###
 
 data = pd.read_csv("data/ventes.csv")
 
-# figureForRegion = px.pie(data, values="qte", names="region", title="Quantité vendue par région")
-# figureForRegion.write_html("quantity-by-region.html")
+figureForRegion = px.pie(data, values="qte", names="region", title="Quantité vendue par région")
+figureForRegion.write_html("quantity-by-region.html")
 
 
-# figureForProduct = px.pie(data, values="qte", names="produit", title="Quantité vendue par produit")
-# figureForProduct.write_html("quantity-by-product.html")
+figureForProduct = px.pie(data, values="qte", names="produit", title="Quantité vendue par produit")
+figureForProduct.write_html("quantity-by-product.html")
 
 
-# webbrowser.open("quantity-by-region.html")
-# webbrowser.open("quantity-by-product.html")
+webbrowser.open("quantity-by-region.html")
+webbrowser.open("quantity-by-product.html")
+
+print("Done!")
